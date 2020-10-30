f6425d0d8699d80bd0c3b9d9cc624cce15239481
AstrobioMike <michael.lee0517@gmail.com>
2020-10-30 14:07:00 -0700

    updating processing info for amplicon GLDS-191

--- Amplicon/GLDS_Processing_Scripts/GLDS-191/full-command-line-log.sh
+++ Amplicon/GLDS_Processing_Scripts/GLDS-191/full-command-line-log.sh
@@ -22,19 +22,19 @@ rm ../Raw_Data/*fastqc*
 
 
  # running cutadapt to remove primers:
-for sample in $(cat unique-sample-IDs.txt); do cutadapt -g CCTACGGGNGGCWGCAG -G GACTACHVGGGTATCTAATCC -o ../Trimmed_Reads/${sample}-R1-primer-trimmed.fastq.gz -p ../Trimmed_Reads/${sample}-R2-primer-trimmed.fastq.gz --discard-untrimmed -m 250 ../Raw_Data/${sample}_R1_raw.fastq.gz ../Raw_Data/${sample}_R2_raw.fastq.gz; done > ../Trimmed_Reads/cutadapt.log 2>&1
+for sample in $(cat unique-sample-IDs.txt); do cutadapt -g CCTACGGGNGGCWGCAG -G GACTACHVGGGTATCTAATCC -o ../Trimmed_Sequence_Data/${sample}-R1-primer-trimmed.fastq.gz -p ../Trimmed_Sequence_Data/${sample}-R2-primer-trimmed.fastq.gz --discard-untrimmed -m 250 ../Raw_Data/${sample}_R1_raw.fastq.gz ../Raw_Data/${sample}_R2_raw.fastq.gz; done > ../Trimmed_Sequence_Data/cutadapt.log 2>&1
 
-cat <( printf "sample\traw_reads\tcutadapt_trimmed\n" ) <( paste unique-sample-IDs.txt <( grep "read pairs processed" ../Trimmed_Reads/cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) <( grep "Pairs written" ../Trimmed_Reads/cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) ) > ../Trimmed_Reads/trimmed-read-counts.tsv
+cat <( printf "sample\traw_reads\tcutadapt_trimmed\n" ) <( paste unique-sample-IDs.txt <( grep "read pairs processed" ../Trimmed_Sequence_Data/cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) <( grep "Pairs written" ../Trimmed_Sequence_Data/cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) ) > ../Trimmed_Sequence_Data/trimmed-read-counts.tsv
 
   # processing in R (quality filtering, ASV processing, taxonomic classification, and generating primary output files)
 Rscript full-R-processing.R
 
   # running fastqc on filtered reads
-fastqc ../Filtered_Reads/*.gz -t 20
+fastqc ../Filtered_Sequence_Data/*.gz -t 20
   # summarizing fastqc results with multiqc
-multiqc -z -o ../FastQC_Outputs/ ../Filtered_Reads/ --interactive
+multiqc -z -o ../FastQC_Outputs/ ../Filtered_Sequence_Data/ --interactive
   # renaming the outputs
 mv ../FastQC_Outputs/multiqc_data.zip ../FastQC_Outputs/filtered_multiqc_data.zip
 mv ../FastQC_Outputs/multiqc_report.html ../FastQC_Outputs/filtered_multiqc_report.html
   # removing the individual fastqc files now that summarized in multiqc output
-rm ../Filtered_Reads/*fastqc*
+rm ../Filtered_Sequence_Data/*fastqc*
