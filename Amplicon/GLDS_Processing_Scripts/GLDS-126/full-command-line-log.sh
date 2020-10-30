36de04b2900287eb0acbab2dae87df5a919754cb
AstrobioMike <michael.lee0517@gmail.com>
2020-10-30 14:02:37 -0700

    updating processing info for amplicon GLDS-126

--- Amplicon/GLDS_Processing_Scripts/GLDS-126/full-command-line-log.sh
+++ Amplicon/GLDS_Processing_Scripts/GLDS-126/full-command-line-log.sh
@@ -24,13 +24,13 @@ rm ../Raw_Data/*fastqc*
 
   # running cutadapt to remove primers:
     # 16S-universal
-for sample in $(cat 16S-universal-unique-sample-IDs.txt); do cutadapt -a ^GTGCCAGCMGCCGCGGTAA...ATTAGAWACCCBDGTAGTCC -A ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC -o ../Trimmed_Reads/${sample}-R1-primer-trimmed.fastq.gz -p ../Trimmed_Reads/${sample}-R2-primer-trimmed.fastq.gz ../Raw_Data/${sample}_R1_raw.fastq.gz ../Raw_Data/${sample}_R2_raw.fastq.gz --discard-untrimmed -m 100; done > ../Trimmed_Reads/16S-universal-cutadapt.log 2>&1
+for sample in $(cat 16S-universal-unique-sample-IDs.txt); do cutadapt -a ^GTGCCAGCMGCCGCGGTAA...ATTAGAWACCCBDGTAGTCC -A ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC -o ../Trimmed_Sequence_Data/${sample}-R1-primer-trimmed.fastq.gz -p ../Trimmed_Sequence_Data/${sample}-R2-primer-trimmed.fastq.gz ../Raw_Data/${sample}_R1_raw.fastq.gz ../Raw_Data/${sample}_R2_raw.fastq.gz --discard-untrimmed -m 100; done > ../Trimmed_Reads/16S-universal-cutadapt.log 2>&1
       # keeping track of reads maintained (for some reason there are some very short sequences mixed in with the "16S_dust_FLT_uni_noINCd_VAC_2008_RISS2" sample. these are being dropped here. not sure how they are here as these should all be the same length if not yet manipulated)
-cat <( printf "sample\traw_reads\tcutadapt_trimmed\n" ) <( paste 16S-universal-unique-sample-IDs.txt <( grep "read pairs processed" ../Trimmed_Reads/16S-universal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) <( grep "Pairs written" ../Trimmed_Reads/16S-universal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) ) > ../Trimmed_Reads/16S-universal-trimmed-read-counts.tsv
+cat <( printf "sample\traw_reads\tcutadapt_trimmed\n" ) <( paste 16S-universal-unique-sample-IDs.txt <( grep "read pairs processed" ../Trimmed_Sequence_Data/16S-universal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) <( grep "Pairs written" ../Trimmed_Sequence_Data/16S-universal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) ) > ../Trimmed_Sequence_Data/16S-universal-trimmed-read-counts.tsv
     # 16S-archaeal
-for sample in $(cat 16S-archaeal-unique-sample-IDs.txt); do cutadapt -a ^GYGCASCAGKCGMGAAW...CAGCMGCCGCGGTAA -A ^TTACCGCGGCKGCTG...WTTCKCGMCTGSTGCRC -o ../Trimmed_Reads/${sample}-R1-primer-trimmed.fastq.gz -p ../Trimmed_Reads/${sample}-R2-primer-trimmed.fastq.gz ../Raw_Data/${sample}_R1_raw.fastq.gz ../Raw_Data/${sample}_R2_raw.fastq.gz --discard-untrimmed -m 100; done > ../Trimmed_Reads/16S-archaeal-cutadapt.log 2>&1
+for sample in $(cat 16S-archaeal-unique-sample-IDs.txt); do cutadapt -a ^GYGCASCAGKCGMGAAW...CAGCMGCCGCGGTAA -A ^TTACCGCGGCKGCTG...WTTCKCGMCTGSTGCRC -o ../Trimmed_Sequence_Data/${sample}-R1-primer-trimmed.fastq.gz -p ../Trimmed_Sequence_Data/${sample}-R2-primer-trimmed.fastq.gz ../Raw_Data/${sample}_R1_raw.fastq.gz ../Raw_Data/${sample}_R2_raw.fastq.gz --discard-untrimmed -m 100; done > ../Trimmed_Sequence_Data/16S-archaeal-cutadapt.log 2>&1
       # keeping track of reads maintained
-cat <( printf "sample\traw_reads\tcutadapt_trimmed\n" ) <( paste 16S-archaeal-unique-sample-IDs.txt <( grep "read pairs processed" ../Trimmed_Reads/16S-archaeal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) <( grep "Pairs written" ../Trimmed_Reads/16S-archaeal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) ) > ../Trimmed_Reads/16S-archaeal-trimmed-read-counts.tsv
+cat <( printf "sample\traw_reads\tcutadapt_trimmed\n" ) <( paste 16S-archaeal-unique-sample-IDs.txt <( grep "read pairs processed" ../Trimmed_Sequence_Data/16S-archaeal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) <( grep "Pairs written" ../Trimmed_Sequence_Data/16S-archaeal-cutadapt.log | tr -s " " "\t" | cut -f 5 | tr -d "," ) ) > ../Trimmed_Sequence_Data/16S-archaeal-trimmed-read-counts.tsv
 
   # processing in R (quality filtering, ASV processing, taxonomic classification, and generating primary output files)
     # 16S-universal
@@ -39,11 +39,11 @@ Rscript 16S-universal-full-R-processing.R
 Rscript 16S-archaeal-full-R-processing.R
 
   # running fastqc on filtered reads
-fastqc ../Filtered_Reads/*.gz -t 15
+fastqc ../Filtered_Sequence_Data/*.gz -t 15
   # summarizing fastqc results with multiqc
-multiqc -z -o ../FastQC_Outputs/ ../Filtered_Reads/
+multiqc -z -o ../FastQC_Outputs/ ../Filtered_Sequence_Data/
   # renaming the outputs
 mv ../FastQC_Outputs/multiqc_data.zip ../FastQC_Outputs/filtered_multiqc_data.zip
 mv ../FastQC_Outputs/multiqc_report.html ../FastQC_Outputs/filtered_multiqc_report.html
   # removing the individual fastqc files now that summarized in multiqc output
-rm ../Filtered_Reads/*fastqc*
+rm ../Filtered_Sequence_Data/*fastqc*
