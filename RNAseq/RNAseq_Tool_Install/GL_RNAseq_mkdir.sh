eded625f8cabf6da4596a545cdcc76c8f45cd3ff
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-12-08 01:10:05 -0800

    Add files via upload

---
+++ RNAseq/RNAseq_Tool_Install/GL_RNAseq_mkdir.sh
@@ -0,0 +1,23 @@
+mkdir 00-RawData  01-TG_Preproc  02-STAR_Alignment  03-RSEM_Counts  04-DESeq2_NormCounts  05-DESeq2_DGE  processing_scripts
+mkdir 00-RawData/Fastq
+mkdir 00-RawData/FastQC_Reports
+mkdir 00-RawData/FastQC_Reports/raw_multiqc_report
+
+mkdir 01-TG_Preproc/Fastq
+mkdir 01-TG_Preproc/FastQC_Reports
+mkdir 01-TG_Preproc/FastQC_Reports/trimmed_multiqc_report
+mkdir 01-TG_Preproc/Trimming_Reports
+
+mkdir 05-DESeq2_DGE/ERCC_NormDGE
+
+mkdir processing_scripts/00-RawData
+mkdir processing_scripts/01-TG_Preproc
+mkdir processing_scripts/02-STAR_Alignment
+mkdir processing_scripts/03-RSEM_Counts
+mkdir processing_scripts/04-05-DESeq2_NormCounts_DGE
+
+mkdir processing_scripts/00-RawData/raw_fastqc_out_logs
+mkdir processing_scripts/01-TG_Preproc/TG_out_logs
+mkdir processing_scripts/01-TG_Preproc/trimmed_fastqc_out_logs
+mkdir processing_scripts/02-STAR_Alignment/STAR_align_out_logs
+mkdir processing_scripts/03-RSEM_Counts/rsem_count_out_logs
