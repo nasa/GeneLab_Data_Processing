ad65428e192b7e7a5af0a763ca570cf478bd5981
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-12-09 09:46:05 -0800

    Add files via upload

---
+++ RNAseq/RNAseq_Tool_Install/GL_genome_index_mkdir.sh
@@ -0,0 +1,19 @@
+mkdir ./Genomes
+mkdir ./Genomes/Mus_musculus
+mkdir ./Genomes/Arabidopsis_thaliana
+mkdir ./Genomes/Bacillus_subtilis
+mkdir ./Genomes/Brassica_rapa
+mkdir ./Genomes/Drosophila_melanogaster
+mkdir ./Genomes/ERCC
+mkdir ./Genomes/Escherichia_coli
+mkdir ./Genomes/Homo_sapiens
+mkdir ./Genomes/Mycobacterium_marinum
+mkdir ./Genomes/Oryzias_latipes
+mkdir ./Genomes/Staphylococcus_aures
+mkdir ./Genomes/Streptococcus_mutans
+
+mkdir ./STAR_Indices
+mkdir ./STAR_Indices/STAR_index_scripts
+
+mkdir ./RSEM_Indices
+mkdir ./RSEM_Indices/RSEM_index_scripts
