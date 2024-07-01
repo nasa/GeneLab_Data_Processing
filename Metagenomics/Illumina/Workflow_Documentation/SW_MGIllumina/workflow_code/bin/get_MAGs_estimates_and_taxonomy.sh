d119d4c5f6ced8413c56d259032480dced46d219
olabiyi <obadbotanist@yahoo.com>
2024-07-01 14:00:59 -0500

    Fixed GTDBTK taxonomy variable issue

--- Metagenomics/Illumina/Workflow_Documentation/SW_MGIllumina/workflow_code/bin/get_MAGs_estimates_and_taxonomy.sh
+++ Metagenomics/Illumina/Workflow_Documentation/SW_MGIllumina/workflow_code/bin/get_MAGs_estimates_and_taxonomy.sh
@@ -2,7 +2,7 @@
 MAGs_dir=$1
 MAG_assembly_summaries=$2
 MAGs_checkm_out=$3
-gtdbtk-out=$4
+gtdbtk_out=$4
 
 # Making sure none of the intermediate files exist already
 rm -rf checkm-estimates.tmp \
@@ -17,7 +17,7 @@ for MAG in $(cut -f 1 ${MAG_assembly_summaries} | tail -n +2); do
 	grep -w -m 1 "^${MAG}" ${MAGs_checkm_out} | \
         cut -f 12,13,14 >> checkm-estimates.tmp
 
-        grep -w "^${MAG}" ${gtdbtk-out}/gtdbtk.*.summary.tsv | \
+        grep -w "^${MAG}" ${gtdbtk_out}/gtdbtk.*.summary.tsv | \
         cut -f 2 | sed 's/^.__//' | \
         sed 's/;.__/\t/g' | \
 	awk 'BEGIN{ OFS=FS="\t" } { for (i=1; i<=NF; i++) if ( $i ~ /^ *$/ ) $i = "NA" }; 1' \
