e910a47cfa46cd90fb7bb6c6db7a27b07d83dd4a
olabiyi <obadbotanist@yahoo.com>
2025-04-03 13:59:35 -0700

    Edit workflow launch scripts

--- Amplicon/Illumina/Workflow_Documentation/NF_AmpIllumina-C/workflow_code/launch.sh
+++ Amplicon/Illumina/Workflow_Documentation/NF_AmpIllumina-C/workflow_code/launch.sh
@@ -1,27 +1,37 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 set  -euo pipefail
 
-# Script to launch the nextflow workflow on a slurm compute cluster
+# Script to launch a nextflow workflow on N288 cluster
 
-# Usage: bash ./launch.sh '[extra arguments]' [main.nf]
-# Example: bash ./launch.sh '--accession GLDS-487 --target_region 16S' path/to/main.nf 
+# Usage: bash ./launch.sh [mode] [main.nf]  [config] '[extra arguments]'
+# Examples
+
+# Processing:
+#      bash ./launch.sh processing path/to/main.nf path/to/nextflow.config '--accession GLDS-487 --target_region 16S'
+
+# Postprocessing:
+#      bash ./launch.sh post_processing  path/to/post_processing.nf  path/to/post_processing.config \
+#           '--name First M. Last --email name@domain.com --GLDS_accession GLDS-487 --OSD_accession ODS-487 --isa_zip  ../GeneLab/OSD-487_metadata_GLDS-487-ISA.zip --runsheet ../GeneLab/GLfile.csv'
 
 
-EXTRA=${1:-""}  # extra arguments to the nextflow run command
-MAIN=${2:-"main.nf"} # path to the main.nf nextflow script
+
+MODE=${1:-''} # Script run mode i.e. processing or post_processing.
+MAIN=${2:-''}  # Path to main.nf or post_processing.nf nextflow script for processing and post_processing, respectively.
+CONFIG=${3:-''} # nextflow config file i.e. nextflow.config or post_processing.config.
+EXTRA=${4:-''}  # extra arguments to the nextflow run command particularly those in the params sections of the .config files.
 
 
 #==============================================================================
 # SETUP START
 #==============================================================================
 eval "$(conda shell.bash hook)"
-conda activate /path/to/envs/nextflow
+conda activate /path/to/conda/envs/nextflow
 export NXF_SINGULARITY_CACHEDIR=/path/to/singularity_images/
 export TOWER_ACCESS_TOKEN=<YOUR ACCESS TOKEN>
 export TOWER_WORKSPACE_ID=<YOUR WORKSPACE ID>
 
 #==============================================================================
-# UMASK CONFIGURATION 
+# UMASK CONFIGURATION
 #==============================================================================
 echo "Setting umask to enable group read-access by default"
 umask u=rwx,g=rx
@@ -31,10 +41,12 @@ echo "Umask settings for this launch: $(umask -S)"
 #==============================================================================
 # NEXTFLOW COMMAND START
 #==============================================================================
+if [ ${MODE} == "processing" ]; then
 
-RUN_NAME=MAIN_$(date +%Y%m%d%H%M%S)
+    RUN_NAME=MAIN_$(date +%Y%m%d%H%M%S)
 
-RUN_COMMAND="nextflow run \
+    RUN_COMMAND="nextflow -C ${CONFIG}
+                    run \
                     -name ${RUN_NAME} \
                     ${MAIN} \
                     -resume \
@@ -44,19 +56,48 @@ RUN_COMMAND="nextflow run \
                     -ansi-log false \
                     ${EXTRA}"
 
-echo "Running command: ${RUN_COMMAND}"
-echo ""
-[ -d processing_scripts ] || mkdir processing_scripts
-eval ${RUN_COMMAND} && echo ${RUN_COMMAND} > processing_scripts/command.txt
+    echo "Running command: ${RUN_COMMAND}"
+    echo ""
+    [ -d processing_scripts ] || mkdir processing_scripts
+    eval ${RUN_COMMAND} && echo ${RUN_COMMAND} > processing_scripts/command.txt
 
-# Save the nextflow log to a file
-echo "Creating Nextflow processing info file..."
-nextflow log ${RUN_NAME} -f name,script > processing_scripts/nextflow_processing_info_GLAmpliseq.txt
-echo nextflow log ${RUN_NAME} -f name,script >> processing_scripts/nextflow_processing_info_GLAmpliseq.txt
-echo "Nextflow processing info written to processing_scripts/nextflow_processing_info_GLAmpliseq.txt"
- 
+    # Save the nextflow log to a file
+    echo "Creating Nextflow processing info file..."
+    nextflow log ${RUN_NAME} -f name,script > processing_scripts/nextflow_processing_info_GLAmpliseq.txt
+    echo nextflow log ${RUN_NAME} -f name,script >> processing_scripts/nextflow_processing_info_GLAmpliseq.txt
+    echo "Nextflow processing info written to processing_scripts/nextflow_processing_info_GLAmpliseq.txt"
+
+
+elif [ ${MODE} == "post_processing" ];then
+
+
+    RUN_NAME=POST_$(date +%Y%m%d%H%M%S)
+
+    RUN_COMMAND="nextflow -C ${CONFIG}
+                    run \
+                    -name ${RUN_NAME} \
+                    ${MAIN} \
+                    -resume \
+                    -profile slurm,singularity \
+                    -with-tower \
+                    -process.queue 'normal' \
+                    -ansi-log false \
+                    ${EXTRA}"
+
+    echo "Running command: ${RUN_COMMAND}"
+    echo ""
+    eval ${RUN_COMMAND}
+
+else
+    echo 'Please provide a valid mode to run the workflow.'
+    echo 'Either processing or post_processing for running the processing or post_processing workflows, respectively.'
+    exit 1
+fi
+
+
 # Set permissions on launch directory
 echo ""
 echo "Setting permissions on launch directory..."
 chmod -R 755 .
 echo "Permissions set to 755 recursively on launch directory"
+                                                                                            
